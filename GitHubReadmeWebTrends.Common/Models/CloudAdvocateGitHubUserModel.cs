bc53bc5de092545e34748a0f3b28a7c0e825c73d
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-10-05 12:07:15 -0700

    Update CloudAdvocateGitHubUserModel.cs

--- GitHubReadmeWebTrends.Common/Models/CloudAdvocateGitHubUserModel.cs
+++ GitHubReadmeWebTrends.Common/Models/CloudAdvocateGitHubUserModel.cs
@@ -2,11 +2,17 @@
 {
     public class CloudAdvocateGitHubUserModel
     {
-        public CloudAdvocateGitHubUserModel(in string fullName, in string gitHubUserName, in string microsoftAlias) =>
-            (FullName, GitHubUserName, MicrosoftAlias) = (fullName, gitHubUserName, microsoftAlias);
+        public CloudAdvocateGitHubUserModel(in string fullName, in string gitHubUserName, in string microsoftAlias, in string team)
+        {
+            FullName = fullName;
+            GitHubUserName = gitHubUserName;
+            MicrosoftAlias = microsoftAlias;
+            Team = team;
+        }
 
         public string FullName { get; }
         public string GitHubUserName { get; }
         public string MicrosoftAlias { get; }
+        public string Team { get; }
     }
 }
